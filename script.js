d94e779a4b216edcd24c9b56185da03de3a708ff
Fatema Althawadi <codebyfatem@gmail.com>
2026-01-10 20:18:15 +0300

    feat: add computer and human choice logic

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
-//hello.js
-console.log("Hello World!");
+function getComputerChoice() {
+	let choice = Math.floor(Math.random() * 3); //this will generate either 0,1 or 2
+	if (choice === 0) {
+		return "rock";
+	} else if (choice === 1) {
+		return "paper";
+	} else if (choice === 2) {
+		return "scissors";
+	} else {
+		throw new Error("Undefined Behaviour");
+	}
+}
+try {
+	console.log("Computer Choose: " + getComputerChoice());
+} catch (e) {
+	console.log("An Error Occured: " + e.message);
+}
+
+function getHumanChoice() {
+	let choice = prompt("Enter your choice (rock, paper or scissors): ");
+	if (choice.toLowerCase() === "rock") {
+		return "rock";
+	} else if (choice.toLowerCase() === "paper") {
+		return "paper";
+	} else if (choice.toLowerCase() === "scissors") {
+		return "scissors";
+	} else {
+		throw new Error("Undefined Behaviour");
+	}
+}
+try {
+	console.log("Your Choice: " + getHumanChoice());
+} catch (e) {
+	console.log("An Error Occured: " + e.message);
+}
+
