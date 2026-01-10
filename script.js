0c607ed05e88c002fde370795d027fdd97c25366
Fatema Althawadi <codebyfatem@gmail.com>
2026-01-10 21:42:32 +0300

    feat: add scores and rounds logic

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ function getComputerChoice() {
 	} else if (choice === 2) {
 		return "scissors";
 	} else {
-		throw new Error("Undefined Behaviour");
+		throw new Error("Undefined Behaviour.");
 	}
 }
-try {
-	console.log("Computer Choose: " + getComputerChoice());
-} catch (e) {
-	console.log("An Error Occured: " + e.message);
-}
+// try {
+// 	console.log("Computer Choose: " + getComputerChoice());
+// } catch (e) {
+// 	console.log("An Error Occured: " + e.message);
+// }
 
 function getHumanChoice() {
 	let choice = prompt("Enter your choice (rock, paper or scissors): ");
@@ -25,12 +25,47 @@ function getHumanChoice() {
 	} else if (choice.toLowerCase() === "scissors") {
 		return "scissors";
 	} else {
-		throw new Error("Undefined Behaviour");
+		throw new Error("Input is Invalid. Please try again.");
 	}
 }
-try {
-	console.log("Your Choice: " + getHumanChoice());
-} catch (e) {
-	console.log("An Error Occured: " + e.message);
+// try {
+// 	console.log("Your Choice: " + getHumanChoice());
+// } catch (e) {
+// 	console.log("An Error Occured: " + e.message);
+// }
+
+var humanScore = 0;
+var computerScore = 0;
+var round = 0;
+
+function playRound(humanChoice, computerChoice){
+    let player = humanChoice.toLowerCase();
+    let playerWins = true;
+    let computerWins = false;
+    if(player === computerChoice){
+       console.log("Tie");
+    }else if(player === "rock" && computerChoice ==="scissors"){
+        console.log("You win! Rock beats Scissors!");
+    }else if(player === "paper" && computerChoice==="rock"){
+        console.log("You win! Paper beats Rock!");
+    }else if(player === "scissors" && computerChoice === "paper"){
+        console.log("You win! Scissors beats Paper!");
+    }else{
+        console.log("You lose! Paper beats Rock!");
+        playerWins= false;
+        computerWins= true;
+    }
+
+    if(computerWins){
+        computerScore++;
+    }else{
+        humanScore++;
+    }
+    round++;
+    return;
 }
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
